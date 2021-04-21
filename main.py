12d3b8d221e0c1b55ff9bf589379e1e0bc742275
Vitali Baranov <vitalib.freeman@gmail.com>
2021-04-21 15:29:16 +0300

    Hide credentials

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import logging
+import os
 import time
 
 import requests
 import telegram
+from dotenv import load_dotenv
 from requests.exceptions import ConnectionError, Timeout
 
-#TODO store securely
-TOKEN = '46831f92d5bb3c243ac1fa7ce1d9774d59a6f89c'
-TELEGRAM_TOKEN = '1756378228:AAHnmYX-KGz5PkEdgz-1hHXhunEp3M7948I'
-SUBMITTED_TASKS_URL = 'https://dvmn.org/api/user_reviews/'
-LONG_POLLING_TASKS_URL = 'https://dvmn.org/api/long_polling/'
-TELEGRAM_RESULT_MESSAGE = "У вас проверили работу {}\n{}"
+DEVMAN_ATTEMPTS_URL = 'https://dvmn.org/api/long_polling/'
 
-def getFromDmvnApi(url, token, timestamp, timeout=90):
-    headers = {'Authorization': f'Token {TOKEN}'}
+
+def fetch_devman_attempts(url, devman_toeken, timestamp, timeout=90):
+    headers = {'Authorization': f'Token {devman_toeken}'}
     payload = {
         'timestamp': timestamp
     }
     while True:
         try:
-            response = requests.get(url, headers=headers, params=payload, timeout=timeout)
+            response = requests.get(
+                url,
+                headers=headers,
+                params=payload,
+                timeout=timeout
+            )
             response.raise_for_status()
             break
         except (Timeout, ConnectionError) as e:
@@ -28,29 +31,37 @@ def getFromDmvnApi(url, token, timestamp, timeout=90):
             continue
     return response.json()
 
-def convert(attempt):
-    positive_result = 'Преподатавателю все понравилось, можно приступать к следующему уроку! '
+
+def convert_message(attempt):
+    positive_result = 'Преподатавателю все понравилось, можно приступать \
+         к следующему уроку! '
     negative_result = 'К сожалению в работе нашлись ошибки! '
     return "У вас проверили работу <a href='{}'>{}</a>\n\n{}".format(
         "https://dvmn.org{}".format(attempt['lesson_url']),
-        attempt['lesson_title'], 
+        attempt['lesson_title'],
         negative_result if attempt['is_negative'] else positive_result,
     )
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    logging.info(bot.get_me())
+    load_dotenv()
+    devman_api_token = os.getenv('DEVMAN_API_TOKEN')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
     timestamp = time.time()
-    chat_id = 1109991497 #TODO fetch from settings
     while True:
-        response = getFromDmvnApi(LONG_POLLING_TASKS_URL, TOKEN, timestamp)
-        logging.info(response)
+        response = fetch_devman_attempts(
+            DEVMAN_ATTEMPTS_URL,
+            devman_api_token,
+            timestamp
+        )
         if (response['status'] == 'found'):
             for attempt in response['new_attempts']:
-                bot.send_message(chat_id, text=convert(attempt), parse_mode=telegram.ParseMode.HTML)
+                bot.send_message(
+                    telegram_chat_id,
+                    text=convert_message(attempt),
+                    parse_mode=telegram.ParseMode.HTML
+                )
             timestamp = response['last_attempt_timestamp']
         else:
             timestamp = response['timestamp_to_request']
-        logging.debug(timestamp)
